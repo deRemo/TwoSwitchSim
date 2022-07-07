4969636db561203d5baefc96bf5b3a1f62efde0d
Remo <andreoliremo@gmail.com>
2022-07-07 16:37:30 +0200

    clarified event print statement

--- main.cpp
+++ main.cpp
@@ -198,7 +198,7 @@ void schedule_departure_event_from(q_info_t* q){
 }
 
 void arrival_event(q_info_t* q) {
-    std::cout << "arrival   " << q->name << "  (" << sim_clock << ")" << std::endl;
+    std::cout << "arrival   " << q->name << "  (sim. time: " << sim_clock << ")" << std::endl;
 
     if (q->status == BUSY) { //Packet cannot be processed immediately, keep it pending
         q->n_pkts += 1;
@@ -222,7 +222,7 @@ void arrival_event(q_info_t* q) {
 }
 
 void departure_event(q_info_t* q) {
-    std::cout << "departure " << q->name << "  (" << sim_clock << ")" << std::endl;
+    std::cout << "departure " << q->name << "  (sim. time: " << sim_clock << ")" << std::endl;
 
     //finished serving one packet
     if (q->next) { //immediate arrival to the next queue (if any)
