154ab340e6d1e215919de53052a5f4bb8d3c005c
Remo <andreoliremo@gmail.com>
2022-08-05 01:31:08 +0200

    added multiple inverse functions

--- plot_trucated_expon_pdf_cdf.py
+++ plot_trucated_expon_pdf_cdf.py
@@ -19,23 +19,40 @@ plt.rc('xtick', labelsize=18)
 plt.rc('ytick', labelsize=12)
 
 
+def inv_expon_CDF(u):
+    print("lol")
+    return -MEAN * log(u);
+
 #Inverse (doubly) trucated negative exponential cdf
 #(As defined in the simulator)
-def inv_truncated_expon_CDF(u):
-	return A - (log(1 - u * (1 - exp((A - B) / MEAN)))) * MEAN
+#Derived by me starting by a cdf found online
+def inv_truncated_expon_CDF1(u):
+    print("lol2")
+    return A - MEAN * (log(1 - (u * (1 - exp((A - B) / MEAN)))))
 
+
+#Completely derived by me
+def inv_truncated_expon_CDF2(u):
+    print("lol3")
+    return -MEAN * log(exp(-A/MEAN) - (exp(-A/MEAN) - exp(-B/MEAN)) * u)
 
 #Generate random uniform number (Inverse-Transform)
 nums = []
 
-for i in range(N):
-	u = np.random.uniform(low=0.0, high=1.0)
-	nums.append(inv_truncated_expon_CDF(u))
+for inv_f,name in zip([inv_expon_CDF, inv_truncated_expon_CDF1, inv_truncated_expon_CDF2], ["inv_expon", "inv_t_expon1", "inv_t_expon2"]):
+    for i in range(N):
+        u = np.random.uniform(low=0.0, high=1.0)
 
-#Plot generated pdf and save figure
-plt.hist(nums, bins=BINS, density=True, color="antiquewhite", edgecolor="black")
-plt.savefig(f"truncated_expon_pdf.pdf")
+        nums.append(inv_f(u))
+    
+    #Plot generated pdf and save figure
+    plt.hist(nums, bins=BINS, density=True, color="antiquewhite", edgecolor="black")
+    plt.title(f"{name} pdf")
+    plt.savefig(f"{name}_pdf_plot.pdf")
+    plt.show()
 
-#Plot generated cdf and save figure
-plt.hist(nums, bins=BINS, density=True, cumulative=True, color="antiquewhite", edgecolor="black")
-plt.savefig(f"truncated_expon_cdf.pdf")
+    #Plot generated cdf and save figure
+    plt.hist(nums, bins=BINS, density=True, cumulative=True, color="antiquewhite", edgecolor="black")
+    plt.title(f"{name} cdf")
+    plt.savefig(f"{name}_cdf_plot.pdf")
+    plt.show()
